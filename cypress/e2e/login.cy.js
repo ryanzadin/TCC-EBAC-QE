f69118f09bab1f8ab3975eebf274363a1237eaf6
Ryan Dulko <ryandulko458@gmail.com>
2024-10-17 13:48:29 -0300

    logins

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,15 @@
+describe('Fluxo de login', () => {
+    /* Como cliente da EBAC-SHOP
+       Quero fazer o login (autenticação) na plataforma
+       Para visualizar meus pedidos
+
+Regras de negócio:
+• Somente usuários ativos podem fazer login;
+• Deve exibir uma mensagem de erro caso o usuário erre o login e senha;
+• Se o usuário errar por 3 vezes a senha, deve travar por 15 minutos o login */
+
+});
+
 let dadosLogin 
 
     beforeEach(() => {
@@ -12,9 +24,28 @@ context('Funcionalidade Login', () => {
     });
 });
 
-it('deve fazer login', () => {
+it('Deve fazer login com sucesso sendo um usuario ativo', () => {
     cy.fixture('perfil').then((dados) => {
         cy.login(dados.usuario, dados.senha)
-        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
     })
+});
+
+it('Deve exibir uma mensagem de erro caso o usuário erre o login e senha', () => {
+    cy.fixture('perfil').then((dados) => {
+        cy.login(dados.usuario, '123489723')
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+});
+
+it.only('Se o usuário errar por 3 vezes a senha, deve travar por 15 minutos o login', () => {
+    cy.fixture('perfil').then((dados) => {
+        cy.login(dados.usuario, '123489723')
+        cy.get('#password').type('wewedad')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('#password').type('w3232232d')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    });
+   
 });
