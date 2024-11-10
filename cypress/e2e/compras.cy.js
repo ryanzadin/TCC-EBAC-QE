95051eb0489d1631c94478577c486fe11a639422
Ryan Dulko <ryandulko458@gmail.com>
2024-11-10 17:28:31 -0300

    ok

--- cypress/e2e/compras.cy.js
+++ cypress/e2e/compras.cy.js
@@ -70,6 +70,38 @@ describe('Fluxo de Compras', () => {
         })
        
     });
+    it('Tesste de valor ultrapassando R$ 990,00 e cupom não funcionando ', () => {
+        cy.get('.icon-user-unfollow').click()
+        cy.fixture('perfil').then((dados) => {
+            cy.login(dados.usuario, dados.senha)
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
+        cy.visit('/produtos/')
+        cy.fixture('produtos').then(dados => {
+            adcionarProdutoPage.buscarProdutoLista(dados[0].nomeProduto)
+            adcionarProdutoPage.addProdutoCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade)
+            cy.visit('/produtos/')
+            adcionarProdutoPage.buscarProdutoLista(dados[1].nomeProduto)
+            adcionarProdutoPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+            cy.visit('/produtos/')
+            adcionarProdutoPage.buscarProdutoLista(dados[2].nomeProduto)
+            adcionarProdutoPage.addProdutoCarrinho(dados[2].tamanho, dados[2].cor, dados[2].quantidade)
+            cy.visit('/produtos/')
+            adcionarProdutoPage.buscarProdutoLista(dados[3].nomeProduto)
+            adcionarProdutoPage.addProdutoCarrinho(dados[3].tamanho, dados[3].cor, dados[3].quantidade)
+            cy.visit('/produtos/')
+            adcionarProdutoPage.buscarProdutoLista(dados[4].nomeProduto)
+            adcionarProdutoPage.addProdutoCarrinho(dados[4].tamanho, dados[4].cor, dados[4].quantidade)
+            cy.get('.woocommerce-message > .button').click()
+            cy.fixture('cupom').then(( dados => { cupomPage.adcionarCupom(dados[1].cupom)}))
+            cy.get('.checkout-button').click()
+            cy.get('#terms').click()
+            cy.get('#place_order').click()
+            cy.wait(4000)
+            cy.get('.woocommerce-column__title').should('contain', 'Endereço de faturamento')
+        })
+       
+    });
 });
 
 
