0f734c2a3ccf7c297ea3ae1102637c5e84be6726
Ryan Dulko <ryandulko458@gmail.com>
2025-01-25 17:32:28 -0300

    report

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,14 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
+
 module.exports = {
   e2e: {
     // We've imported your old cypress plugins here.
